868ede633340de61f75fa96136bacbc9910f006d
mehmetkesik <mhmtkskx@gmail.com>
2025-09-07 20:59:20 +0300

    updated

---
+++ snippets/examples/if-else.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("if else examples");
+}
---
+++ snippets/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    println!("rshtml snippets examples");
+    println!("run examples 'cargo run --example if-else'");
+    println!("examples in examples folder");
+}
