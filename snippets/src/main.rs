39f6dc8a2c57b0b7f8a8e0980590e9f7d7b99ea8
mehmetkesik <mhmtkskx@gmail.com>
2025-09-08 15:25:24 +0300

    updated

--- snippets/src/main.rs
+++ snippets/src/main.rs
@@ -1,5 +1,29 @@
 fn main() {
-    println!("rshtml snippets examples");
-    println!("run examples 'cargo run --example if-else'");
-    println!("examples in examples folder");
+    println!("Welcome to the rshtml snippets examples!");
+    println!("This project demonstrates various features of the rshtml templating engine.");
+    println!("You can run individual examples to see how they work.");
+    println!();
+    println!("To run an example, use the following command:");
+    println!("  cargo run --example <example_name>");
+    println!();
+    println!("For instance, to run the 'if-else' example, you would use:");
+    println!("  cargo run --example if-else");
+    println!();
+    println!("Here are the available examples:");
+    println!("- code-block");
+    println!("- comment");
+    println!("- component");
+    println!("- escaping");
+    println!("- for");
+    println!("- functions");
+    println!("- if-else");
+    println!("- include");
+    println!("- layout");
+    println!("- match");
+    println!("- parantheses-expression");
+    println!("- raw-block");
+    println!("- simple-expression");
+    println!("- while");
+    println!();
+    println!("Each example corresponds to a view in the 'views' directory.");
 }
